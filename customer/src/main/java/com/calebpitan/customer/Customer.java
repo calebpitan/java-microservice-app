4292da20a9fbe3becd7a3b90e79e250930793b3f
Caleb Adepitan <calebpitan@outlook.com>
2022-11-06 06:00:30 +0100

    Remove "@Data" annotations against strongly advised performance impact
    - Replace @Data with other relevant annotations with better performance
    - Make sure required fields aren't null using a @NonNull checker

--- customer/src/main/java/com/calebpitan/customer/Customer.java
+++ customer/src/main/java/com/calebpitan/customer/Customer.java
@@ -2,8 +2,13 @@ package com.calebpitan.customer;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.Setter;
+import lombok.ToString;
+
+import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,7 +16,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -28,8 +37,26 @@ public class Customer {
             generator = "customer_id_sequence"
     )
     private Integer id;
+
+    @NonNull
     private String firstName;
+
+    @NonNull
     private String lastName;
+
+    @NonNull
     private String email;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Customer customer = (Customer) o;
+        return id != null && Objects.equals(id, customer.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
